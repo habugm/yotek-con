dde5ed63f49aae11d7508694210c12814e784d1c
habugm <haben.gebremedhin@aau.edu.et>
2025-09-15 22:02:46 +0300

    contact

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -141,45 +141,45 @@ Yotek Construction PLC delivers real estate projects that blend functionality, m
     {/* Project 1 */}
     <div className="col-6 col-lg-3 text-center mb-4">
       <img src="assets/img/projects/project1.jpg" className="img-fluid rounded mb-2" alt="Project 1" />
-      <h6>Project Name 1</h6>
+      <h6>Addis Ababa University Forum</h6>
     </div>
 
     {/* Project 2 */}
     <div className="col-6 col-lg-3 text-center mb-4">
       <img src="assets/img/projects/project2.jpg" className="img-fluid rounded mb-2" alt="Project 2" />
-      <h6>Project Name 2</h6>
+      <h6>Sendafa Police College</h6>
     </div>
 
     {/* Project 3 */}
     <div className="col-6 col-lg-3 text-center mb-4">
       <img src="assets/img/projects/construction-1.jpg" className="img-fluid rounded mb-2" alt="Project 3" />
-      <h6>Project Name 3</h6>
+      <h6>Arbaminch Secondary School</h6>
     </div>
 
     {/* Project 4 */}
     <div className="col-6 col-lg-3 text-center mb-4">
       <img src="assets/img/projects/construction-2.jpg" className="img-fluid rounded mb-2" alt="Project 4" />
-      <h6>Project Name 4</h6>
+      <h6>Mekelle Regional Office</h6>
     </div>
 
     <div className="col-6 col-lg-3 text-center mb-4">
-      <img src="assets/img/projects/remodeling-3.jpg" className="img-fluid rounded mb-2" alt="Project 3" />
-      <h6>Project Name 5</h6>
+      <img src="assets/img/projects/construction-3.jpg" className="img-fluid rounded mb-2" alt="Project 3" />
+      <h6>Arbaminch University LOT II</h6>
     </div>
 
     <div className="col-6 col-lg-3 text-center mb-4">
       <img src="assets/img/projects/completed1.jpg" className="img-fluid rounded mb-2" alt="Project 3" />
-      <h6>Project Name 6</h6>
+      <h6>Meles Zenawi Leadership Academy</h6>
     </div>
 
     <div className="col-6 col-lg-3 text-center mb-4">
       <img src="assets/img/projects/completed2.jpg" className="img-fluid rounded mb-2" alt="Project 3" />
-      <h6>Project Name 7</h6>
+      <h6>Kombolcha Air Field</h6>
     </div>
 
     <div className="col-6 col-lg-3 text-center mb-4">
-      <img src="assets/img/projects/construction-1.jpg" className="img-fluid rounded mb-2" alt="Project 3" />
-      <h6>Project Name 8</h6>
+      <img src="assets/img/projects/completed3.jpg" className="img-fluid rounded mb-2" alt="Project 3" />
+      <h6>Gast Mekelle Building Project</h6>
     </div>
 
   </div>
@@ -295,6 +295,67 @@ Yotek Construction PLC delivers real estate projects that blend functionality, m
     </div>
   </div>
 </section>
+<section id="contact" className="contact section">
+  <div className="container section-title">
+      <h2>Contact US</h2>
+      <p> Contact US with the following Informations</p>
+    </div>
+  <div className="container">
+    <div className="row gy-4">
+      <div className="col-lg-6">
+        <div className="info-item d-flex flex-column justify-content-center align-items-center">
+          <i className="bi bi-geo-alt" />
+          <h3>Address</h3>
+          <p>A108 Adam Street, New York, NY 535022</p>
+        </div>
+      </div>{/* End Info Item */}
+      <div className="col-lg-3 col-md-6">
+        <div className="info-item d-flex flex-column justify-content-center align-items-center">
+          <i className="bi bi-telephone" />
+          <h3>Call Us</h3>
+          <p>+1 5589 55488 55</p>
+        </div>
+      </div>{/* End Info Item */}
+      <div className="col-lg-3 col-md-6">
+        <div className="info-item d-flex flex-column justify-content-center align-items-center">
+          <i className="bi bi-envelope" />
+          <h3>Email Us</h3>
+          <p>info@example.com</p>
+        </div>
+      </div>{/* End Info Item */}
+    </div>
+    <div className="row gy-4 mt-1">
+      <div className="col-lg-6">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d48389.78314118045!2d-74.006138!3d40.710059!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25a22a3bda30d%3A0xb89d1fe6bc499443!2sDowntown%20Conference%20Center!5e0!3m2!1sen!2sus!4v1676961268712!5m2!1sen!2sus" frameBorder={0} style={{border: 0, width: '100%', height: 400}} allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade" />
+      </div>{/* End Google Maps */}
+      <div className="col-lg-6">
+        <form action="forms/contact.php" method="post" className="php-email-form">
+          <div className="row gy-4">
+            <div className="col-md-6">
+              <input type="text" name="name" className="form-control" placeholder="Your Name" required />
+            </div>
+            <div className="col-md-6 ">
+              <input type="email" className="form-control" name="email" placeholder="Your Email" required />
+            </div>
+            <div className="col-md-12">
+              <input type="text" className="form-control" name="subject" placeholder="Subject" required />
+            </div>
+            <div className="col-md-12">
+              <textarea className="form-control" name="message" rows={6} placeholder="Message" required defaultValue={""} />
+            </div>
+            <div className="col-md-12 text-center">
+              <div className="loading">Loading</div>
+              <div className="error-message" />
+              <div className="sent-message">Your message has been sent. Thank you!</div>
+              <button type="submit">Send Message</button>
+            </div>
+          </div>
+        </form>
+      </div>{/* End Contact Form */}
+    </div>
+  </div>
+</section>{/* /Contact Section */}
+
 
 </main>
 
